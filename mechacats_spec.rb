ad17a0b3a61c8a2b025b788277bce4044c29879d
Akshaya Prabakaran <43679591+akshayaprabakaran@users.noreply.github.com>
2021-02-04 04:07:15 -0800

    Remove type parameter and modify to subclass

--- mechacats_spec.rb
+++ mechacats_spec.rb
@@ -5,43 +5,43 @@ require_relative './cats.rb'
 RSpec.describe Mechacats do
   describe '#speed' do
     it 'returns nil' do
-      cat = Mechacats.new(:maine_coon, 1, 1, false)
+      cat = Maine_coon.new(1, 1, false)
 
       expect(cat.speed).to eq(nil)
     end
 
     it 'returns 12' do
-      cat = Mechacats.new(:orange_tabby, 1, 1, false)
+      cat = Orange_tabby.new(1, 1, false)
 
       expect(cat.speed).to eq(12)
     end
 
     it 'returns 0' do
-      cat = Mechacats.new(:bombay, 5, 1, false)
+      cat = Bombay.new(5, 1, false)
 
       expect(cat.speed).to eq(0)
     end
 
     it 'returns 3' do
-      cat = Mechacats.new(:bombay, 1, 1, false)
+      cat = Bombay.new(1, 1, false)
 
       expect(cat.speed).to eq(3)
     end
 
     it 'returns 0' do
-      cat = Mechacats.new(:somali, 1, 1, true)
+      cat = Somali.new(1, 1, true)
 
       expect(cat.speed).to eq(0)
     end
 
     it 'returns 24' do
-      cat = Mechacats.new(:somali, 1, 3, false)
+      cat = Somali.new(1, 3, false)
 
       expect(cat.speed).to eq(24)
     end
 
     it 'returns 6' do
-      cat = Mechacats.new(:somali, 1, 0.5, false)
+      cat = Somali.new(1, 0.5, false)
 
       expect(cat.speed).to eq(6)
     end
@@ -49,31 +49,31 @@ RSpec.describe Mechacats do
 
   describe '#color' do
     it 'returns the right color' do
-      cat = Mechacats.new(:orange_tabby, 1, 1, true)
+      cat = Orange_tabby.new(1, 1, true)
 
       expect(cat.color).to eq('lightning')
     end
 
     it 'returns the right color' do
-      cat = Mechacats.new(:orange_tabby, 1, 1, false)
+      cat = Orange_tabby.new(1, 1, false)
 
       expect(cat.color).to eq('orange white brown and gray')
     end
 
     it 'returns the right color' do
-      cat = Mechacats.new(:bombay, 1, 1, false)
+      cat = Bombay.new(1, 1, false)
 
       expect(cat.color).to eq('black')
     end
 
     it 'returns the right color' do
-      cat = Mechacats.new(:somali, 1, 6, false)
+      cat = Somali.new(1, 6, false)
 
       expect(cat.color).to eq('orange')
     end
 
     it 'returns the right color' do
-      cat = Mechacats.new(:somali, 1, 4, false)
+      cat = Somali.new(1, 4, false)
 
       expect(cat.color).to eq('orange and white')
     end
