dd587b07f6accdb7943d203b37c2c9a7dac4a1dc
Akshaya Prabakaran <43679591+akshayaprabakaran@users.noreply.github.com>
2021-02-04 03:23:50 -0800

    Bombay's speed computation method

--- cats.rb
+++ cats.rb
@@ -1,2 +1,6 @@
 #subclass Bombay inherits from Mechacats super-class
 class Bombay < Mechacats
+  def speed
+    #call speed computation(catnip)method and find maximum 
+    return [0, compute_catnip_speed].max 
+  end
