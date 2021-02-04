f45f52a4b0cda59a88a0a77acd9eef2615b1e79f
Akshaya Prabakaran <43679591+akshayaprabakaran@users.noreply.github.com>
2021-02-04 04:20:41 -0800

    Organize speed check tests

--- mechacats_spec.rb
+++ mechacats_spec.rb
@@ -3,78 +3,79 @@ require_relative './mechacats'
 require_relative './cats.rb'
 
 RSpec.describe Mechacats do
-  describe '#speed' do
-    it 'returns nil' do
-      cat = Maine_coon.new(1, 1, false)
-
-      expect(cat.speed).to eq(nil)
+   #test suite for verifying speed method
+  describe 'Speed Check' do
+    #tests for Somali's speed method 
+    describe 'Somali' do
+      it 'returns 0' do
+        cat = Somali.new(1, 1, true)
+        expect(cat.speed).to eq(0)
+      end
+      
+      it 'returns 24' do 
+        cat = Somali.new(1, 3, false)
+        expect(cat.speed).to eq(24)
+      end
+      
+      it 'returns 6' do
+        cat = Somali.new(1, 0.5, false)
+        expect(cat.speed).to eq(6)
+      end
     end
+    
+    #tests for Bombay's speed method 
+    describe 'Bombay' do
+      it 'returns 0' do
+        cat = Bombay.new(5, 1, false)
+        expect(cat.speed).to eq(0)
+      end
 
-    it 'returns 12' do
-      cat = Orange_tabby.new(1, 1, false)
-
-      expect(cat.speed).to eq(12)
+      it 'returns 3' do
+        cat = Bombay.new(1, 1, false)
+        expect(cat.speed).to eq(3)
+      end
     end
-
-    it 'returns 0' do
-      cat = Bombay.new(5, 1, false)
-
-      expect(cat.speed).to eq(0)
+    
+    #tests for Orange Tabby's speed method 
+    describe 'Orange Tabby' do
+      it 'returns 12' do
+        cat = Orange_tabby.new(1, 1, false)
+        expect(cat.speed).to eq(12)
+      end
     end
-
-    it 'returns 3' do
-      cat = Bombay.new(1, 1, false)
-
-      expect(cat.speed).to eq(3)
-    end
-
-    it 'returns 0' do
-      cat = Somali.new(1, 1, true)
-
-      expect(cat.speed).to eq(0)
-    end
-
-    it 'returns 24' do
-      cat = Somali.new(1, 3, false)
-
-      expect(cat.speed).to eq(24)
-    end
-
-    it 'returns 6' do
-      cat = Somali.new(1, 0.5, false)
-
-      expect(cat.speed).to eq(6)
+    
+    #tests for Maine Coon's speed method 
+    describe 'Maine Coon' do
+      it 'returns nil' do
+        cat = Maine_coon.new(1, 1, false)
+        expect(cat.speed).to eq(nil)
+      end
     end
   end
 
   describe '#color' do
     it 'returns the right color' do
       cat = Orange_tabby.new(1, 1, true)
-
       expect(cat.color).to eq('lightning')
     end
 
     it 'returns the right color' do
       cat = Orange_tabby.new(1, 1, false)
-
       expect(cat.color).to eq('orange white brown and gray')
     end
 
     it 'returns the right color' do
       cat = Bombay.new(1, 1, false)
-
       expect(cat.color).to eq('black')
     end
 
     it 'returns the right color' do
       cat = Somali.new(1, 6, false)
-
       expect(cat.color).to eq('orange')
     end
 
     it 'returns the right color' do
       cat = Somali.new(1, 4, false)
-
       expect(cat.color).to eq('orange and white')
     end
   end
